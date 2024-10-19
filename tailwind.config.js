77617baec75f6fe77cd9c403e391796e36c6f3a3
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-10-19 12:56:47 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,3 @@ export default withMT({
   plugins: [],
 });
 
-// const withMT = require("@material-tailwind/react/utils/withMT");
-
-// module.exports = withMT({
-//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// });
